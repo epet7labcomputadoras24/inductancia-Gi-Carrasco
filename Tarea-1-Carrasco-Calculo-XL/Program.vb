382185c3b0f4941cf21477f429261e80498b9cec
Gi-Carrasco <carrascogisel3@gmail.com>
2024-08-06 23:17:44 -0300

    tarea-1-carrasco

---
+++ Tarea-1-Carrasco-Calculo-XL/Program.vb
@@ -0,0 +1,18 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Dim Xl As Single
+        Dim F As Single
+        Dim I As Single
+        Dim PI As Single
+        Console.WriteLine("Ingrese el valor de la frecuencia: ")
+        F = Console.ReadLine()
+        Console.WriteLine("Ingrese el valor de la inductancia: ")
+        I = Console.ReadLine()
+        PI = 3.1415
+        Xl = 2 * PI * F * I
+        Console.WriteLine("El valor de Xl es: " & Xl)
+        Console.ReadLine()
+    End Sub
+End Module
